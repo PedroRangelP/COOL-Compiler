cf6d21faaf25accd0ab13d43ef0b353323dc3ce1
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-04-19 15:22:50 -0500

    Initial tests
    Co-Authored-By: Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>

--- listeners/dummy.py
+++ listeners/dummy.py
@@ -11,8 +11,20 @@ class dummyListener(coolListener):
         if ctx.TYPE(0).getText() == 'Main':
             self.main = True
 
+        if ctx.TYPE(0).getText() == 'Int':
+            self.klassInt = True
+
     def exitKlass(self, ctx:coolParser.KlassContext):
         if (not self.main):
             raise nomain()
+        if(self.klassInt):
+            raise badredefineint()
 
+    def enterFeature(self, ctx: coolParser.FeatureContext):
+        if ctx.ID().getText() == 'self':
+            self.hasNamedSelf = True
+
+    def exitFeature(self, ctx: coolParser.FeatureContext):
+        if (self.hasNamedSelf):
+            raise anattributenamedself()
 
