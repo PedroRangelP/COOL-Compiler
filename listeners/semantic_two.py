e0f540d547b4b73f25729efa230ce944140d9b82
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-05-03 20:27:14 -0500

    Badarith, missingclass and redefinedclass
    Co-Authored-By: Alex Helo <itsalexhelo@gmail.com>
    Co-Authored-By: Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>

--- listeners/semantic_two.py
+++ listeners/semantic_two.py
@@ -1,10 +1,13 @@
 from util.exceptions import *
 from util.structure import *
+from util.structure import _allClasses
 from antlr.coolListener import coolListener
 from antlr.coolParser import coolParser
 
 class semanticTwoListener(coolListener):
     def __init__(self):
+        # Clearing classes before every new test
+        _allClasses.clear()
         setBaseKlasses()
 
     def exitExpr_primary(self, ctx: coolParser.Expr_primaryContext):
@@ -20,6 +23,24 @@ class semanticTwoListener(coolListener):
         if ctx.TRUE() or ctx.FALSE():
             ctx.type = 'Bool'
 
-    def enterArith(self, ctx: coolParser.ArithContext):
+    def exitArith(self, ctx: coolParser.ArithContext):
         if ctx.expr(0).type != 'Int' or ctx.expr(1).type != 'Int':
             raise badarith()
+
+    def enterKlass(self, ctx: coolParser.KlassContext):
+        class_type1 = ctx.TYPE(0).getText()
+
+        if class_type1 in _allClasses:
+            raise redefinedclass()
+
+        # If an inherits exists
+        if ctx.TYPE(1):
+            class_type2 = ctx.TYPE(1).getText()
+            
+            if class_type2 not in _allClasses:
+                raise missingclass()
+            else:
+                k = Klass(class_type1, class_type2)
+        else:
+            k = Klass(class_type1)
+        
