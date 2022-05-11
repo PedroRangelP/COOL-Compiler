604e8e729738c4b22a17ae3d15517a2e707233b3
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-05-10 21:01:35 -0500

    Baddispatch test
    Co-Authored-By: Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>

--- listeners/semantic_two.py
+++ listeners/semantic_two.py
@@ -155,7 +155,47 @@ class semanticTwoListener(coolListener):
         for case_stat in ctx.case_stat():
             case_type = case_stat.TYPE().getText()
 
+            # Check if there are multiple branches with the same type
             if case_type in used_types:
                 raise caseidenticalbranch()
             else:
                 used_types.append(case_type)
+    
+    def enterLet_in(self, ctx: coolParser.Let_inContext):
+        symbol_table, current_klass = getScope(ctx)
+
+        # Assign the symbol table to the context of LET_IN
+        ctx.symbol_table = symbol_table
+        ctx.current_klass = current_klass
+
+        # Appending a new dictionary to the array (Opening a scope)
+        ctx.symbol_table.openScope()
+        
+        # Adding the params to the symboltable of this scope
+        for decl in ctx.let_decl():
+            decl_id = decl.ID().getText()
+            decl_type = decl.TYPE().getText()
+            ctx.symbol_table[decl_id] = decl_type
+
+    
+    def exitLet_in(self, ctx: coolParser.Let_inContext):
+        # Pop the last dictionary in the array (Close a scope)
+        ctx.symbol_table.closeScope()
+
+    def enterDispatch(self, ctx: coolParser.DispatchContext):
+        symbol_table, current_klass = getScope(ctx)
+        
+        # Class name: cow
+        klass_name = ctx.expr(0).getText()
+        # Klass type: Animal
+        klass_type = symbol_table[klass_name]
+        # Method name: moo
+        method_name = ctx.ID().getText()
+
+        try:
+            # If the method does not exist for the given class it will raise a KeyError
+            _allClasses[klass_type].lookupMethod(method_name)
+        except:
+            keyErr = True
+
+        if(keyErr): raise baddispatch()
