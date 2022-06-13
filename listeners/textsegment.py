be8322b15d6ad585bef6950693839c0736ed9420
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-06-13 00:03:13 -0500

    Text segment
    Co-Authored-By: Alex Helo <itsalexhelo@gmail.com>
    Co-Authored-By: Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>

--- listeners/textsegment.py
+++ listeners/textsegment.py
@@ -17,19 +17,12 @@ class TextGenerator(coolListener):
             self.result += c.code
         self.result += asm.tpl_end
 
-    def exitPrimaria(self, ctx: coolParser.PrimaryContext):
+    def exitPrimary(self, ctx: coolParser.PrimaryContext):
         self.stack.append(
             asm.tpl_immediate.substitute(immediate=ctx.getText())
         )
 
-    def exitPrimaria_string(self, ctx: coolParser.Primaria_stringContext):
-        self.stack.append(
-            asm.tpl_string_const.substitute(
-                name=ctx.label
-            )
-        )
-
-    def exitSuma(self, ctx: coolParser.SumaContext):
+    def exitSum(self, ctx: coolParser.SumContext):
         self.stack.append(
             asm.tpl_suma.substitute(
                 right=self.stack.pop(),
@@ -37,7 +30,7 @@ class TextGenerator(coolListener):
             )
         )
 
-    def exitResta(self, ctx: coolParser.RestaContext):
+    def exitSubstract(self, ctx: coolParser.SubstractContext):
         self.stack.append(
             asm.tpl_resta.substitute(
                 right=self.stack.pop(),
@@ -45,26 +38,26 @@ class TextGenerator(coolListener):
             )
         )
 
-    def exitAsignacion(self, ctx: coolParser.AsignacionContext):
+    def exitAssignment(self, ctx: coolParser.AssignmentContext):
         ctx.code = asm.tpl_asignacion.substitute(
             prev=self.stack.pop(),
             name=ctx.getChild(0).getText()
         )
 
-    def exitVar(self, ctx: coolParser.VarContext):
-        self.stack.append(
-            asm.tpl_var.substitute(name=ctx.getText())
-        )
+    # def exitVar(self, ctx: coolParser.VarContext):
+    #     self.stack.append(
+    #         asm.tpl_var.substitute(name=ctx.getText())
+    #     )
 
-    def exitPrintint(self, ctx: coolParser.PrintintContext):
-        ctx.code = asm.tpl_print_int.substitute(
-            prev=self.stack.pop()
-        )
+    # def exitPrintint(self, ctx: coolParser.PrintintContext):
+    #     ctx.code = asm.tpl_print_int.substitute(
+    #         prev=self.stack.pop()
+    #     )
 
-    def exitPrintstr(self, ctx: coolParser.PrintstrContext):
-        ctx.code = asm.tpl_print_str.substitute(
-            prev=self.stack.pop()
-        )
+    # def exitPrintstr(self, ctx: coolParser.PrintstrContext):
+    #     ctx.code = asm.tpl_print_str.substitute(
+    #         prev=self.stack.pop()
+    #     )
 
     def exitIf(self, ctx: coolParser.IfContext):
         self.labels = self.labels = self.labels + 1
@@ -74,7 +67,7 @@ class TextGenerator(coolListener):
             stmt_true=ctx.statement(0).code
         )
 
-    def exitIfelse(self, ctx: coolParser.IfelseContext):
+    def exitIf_else(self, ctx: coolParser.If_elseContext):
         self.labels = self.labels = self.labels + 1
         ctx.code = asm.tpl_if_else.substitute(
             prev=self.stack.pop(),
@@ -83,7 +76,7 @@ class TextGenerator(coolListener):
             stmt_false=ctx.statement(1).code
         )
 
-    def exitBlock(self, ctx: coolParser.BlockContext):
+    def exitContent(self, ctx: coolParser.ContentContext):
         ctx.code = ''
         for c in ctx.statement():
             ctx.code += c.code
@@ -96,14 +89,13 @@ class TextGenerator(coolListener):
             stmt=ctx.statement().code
         )
 
-    def exitProcedure(self, ctx: coolParser.ProcedureContext):
-        # No me importan las variables ヽ(°〇°)ﾉ
+    def exitMethod_call(self, ctx: coolParser.Method_callContext):
         ctx.code = asm.tpl_procedure.substitute(
             name=ctx.name.text,
             code=ctx.statement().code
         )
 
-    def exitCall(self, ctx: coolParser.CallContext):
+    def exitDispatch(self, ctx: coolParser.DispatchContext):
         r = ''
         for c in ctx.expression():
             r += self.stack.pop()
@@ -115,6 +107,3 @@ class TextGenerator(coolListener):
                 name=ctx.Variable()
             )
         )
-
-    def exitReturn(self, ctx: coolParser.ReturnContext):
-        ctx.code = self.stack.pop()
